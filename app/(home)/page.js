33aaffe97f0a1f5fa5ecbb35d75d7aa147ecd532
Hojoon Kim <hojoonkim.professional@gmail.com>
2023-11-13 23:25:17 -0800

    nav bar complete - without media

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,16 +1,19 @@
 
 import Image from 'next/image'
+import Link from 'next/link'
 import LinkBar from '@/components/LinkBar'
 import NavBar from '@/components/NavBar';
+import Button from '@/components/Button';
 
 
 export default function HomePage() {
   return (
     <>
      <div>
-      <main>
+      <main className='mt-28 w-full'>
         
-        <div className='flex justify-spacebetween'>
+        {/*First section*/}
+        <div className=' flex justify-between items-center px-10'>
           <div>
             <h1 className="font-bold pb-3 text-5xl text-[#333333]">
               Financial technology <span className="text-[#52DB78]">empowering</span> <span className="text-[#253D84]">consumers and businesses</span>
@@ -18,11 +21,19 @@ export default function HomePage() {
             <p className="text-lg text-[#333333]">
               Omnex provides the financial tools to empower businesses and consumers, with a focus on inclusion of the traditionally underserved.
             </p>
+            <div className='flex pt-5'>
+              <Button className='border-2'>
+                <Link href="link">About Us</Link>
+              </Button>
+              <Button class="btn-green">
+                <Link href="link">Send Money</Link> 
+              </Button>
+            </div>
           </div>
           <div className='flex items-center'>
             <ul className='grid grid-cols-2 gap-3 '>
               <li class="place-self-end">
-                <Image className=' w-96 border-8 overflow-hidden rounded-3xl' 
+                <Image className='border-8 overflow-hidden rounded-3xl' 
                 src="/Webp/AdobeStock_280396247.webp"
                 alt=""
                 width={500}
@@ -30,7 +41,7 @@ export default function HomePage() {
                 />
               </li>
               <li>
-                <Image className='border-8 overflow-hidden rounded-3xl'
+                <Image className='border-8  rounded-3xl'
                 src="/Webp/B-AdobeStock_235833811.webp"
                 alt=""
                 width={500}
@@ -56,6 +67,8 @@ export default function HomePage() {
             </ul>
           </div>
         </div>
+
+        {/*Second section: Explore - What We Do for You*/}
 
       </main>
      </div>
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,9 +1,72 @@
+"use client"
+
+import { useState } from 'react';
 import Link from 'next/link'
-import GreenButton from './GreenButton';
+import Button from '@/components/Button';
 
 export default function NavBar() {
+    const [showAboutDropdown, setShowAboutDropdown] = useState(false);
+    const [showRegulatoryDropdown, setShowRegulatoryDropdown] = useState(false);
+
     return(
-        <nav className='flex justify-between p-5'>
+        <nav className='pt-4'>
+            <div className='flex items-center justify-around'>
+                <div>
+                    <Link className="md:cursor-pointer h-9" href="/">
+                        <img src="/SVG/OmnexLogo.svg" alt="Omnex Logo" width="200"/>
+                    </Link>
+                </div>
+                <ul className='md:flex hidden font-bold items-center gap-10'>
+                    <li>
+                        <div onMouseEnter={() => setShowAboutDropdown(true)}
+                             onMouseLeave={() => setShowAboutDropdown(false)}>
+                            <button>About Us</button>
+                            {showAboutDropdown && (
+                                <ul className="absolute bg-white shadow-md rounded font-normal">
+                                    <li><Link href="/about-us/our-brands">Our Brands</Link></li>
+                                    <li><Link href="/about-us/corporate-press">Corporate Press</Link></li>
+                                    <li><Link href="/about-us/use-cases">Use Cases</Link></li>
+                                </ul>
+                            )}
+                        </div>
+                    </li>
+                    <li>
+                        <Link href="/our-solutions">Our Solutions</Link>
+                    </li>
+                    <li>
+                        <Link href="/careers">Careers</Link>
+                    </li>
+                    <li>
+                        <div onMouseEnter={() => setShowRegulatoryDropdown(true)}
+                             onMouseLeave={() => setShowRegulatoryDropdown(false)}>
+                            <button>Regulatory</button>
+                            {showRegulatoryDropdown && (
+                                <ul className="absolute bg-white shadow-md rounded font-normal">
+                                    <li><Link href="/regulatory/state-licenses">State Licenses</Link></li>
+                                    <li><Link href="/regulatory/privacy-policy">Privacy Policy</Link></li>
+                                    <li><Link href="/regulatory/file-a-complaint">File a Complaint</Link></li>
+                                </ul>
+                            )}
+                        </div>
+                    </li>
+                </ul>
+                <div className='flex items-center gap-3'>
+                    <select className=" bg-white text-black font-bold">
+                        <option>EN</option>
+                        <option>ES</option>
+                    </select>
+                    <Button class="btn-green">
+                        <Link href="link">Send Money</Link> 
+                    </Button>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+
+/*
+<nav className='flex justify-between p-5'>
             <div>
                 <ul className='flex flex-wrap gap-10'>
                     <li>
@@ -44,11 +107,10 @@ export default function NavBar() {
                     <option>EN</option>
                     <option>ES</option>
                 </select>
-                <GreenButton>
+                <Button className="green-button">
                     <Link href="link">Send Money</Link> 
-                </GreenButton>
+                </Button>
             </div>
           
         </nav>
-    );
-}
+*/
