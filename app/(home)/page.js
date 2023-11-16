da3ff72f748ee9c9ba9258ff66008031f6d336c6
Hojoon Kim <hojoonkim.professional@gmail.com>
2023-11-16 11:18:59 -0800

    Success in completing up to first section. minus font and hamburger nav bar

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -12,11 +12,10 @@ export default function HomePage() {
       <main className='mt-0 lg:mt-14 mx-auto w-auto max-w-max'>
         
         {/*First section*/}
-        <div className='flex flex-col-reverse max-w-max lg:flex-row lg:justify-between items-center px-2'>
+        <div className='flex flex-col-reverse lg:flex-row lg:justify-between items-center px-2 w-full'>
           
           {/* Text Area */}
-          <div className='flex-1 min-w-0 
-          '>
+          <div className='flex-1 '>
             <h1 className="font-bold pb-3 text-5xl text-[#333333]">
               Financial technology <span className="text-[#52DB78]">empowering</span> <span className="text-[#253D84]">consumers and businesses</span>
             </h1>
@@ -32,25 +31,19 @@ export default function HomePage() {
               </Button>
             </div>
           </div>
-        
        
           {/* Image Area */}
-          <div>
-            <div className='flex gap-4 pb-14'>
-              <div className='flex flex-col items-end'>
-                <div className='w-36 h-56 lg:w-48 lg:h-72 shrink rounded-[48px] bg-cover bg-no-repeat bg-[center_right_-5rem]' style={{backgroundImage: "url('/Webp/AdobeStock_280396247.webp')"}}/>
-                <div className='w-52 h-32 lg:w-72 lg:h-44 shrink rounded-[48px] bg-cover bg-left mt-4' style={{backgroundImage: "url('/Webp/AdobeStock_409017261.webp')"}}/>
-              </div>
-              
-              <div className='flex flex-col items-start'>
-                <div className='w-40 h-40 lg:w-72 lg:h-56 shrink rounded-[48px] bg-cover bg-no-repeat bg-center' style={{backgroundImage: "url('/Webp/B-AdobeStock_235833811.webp')"}}/>
-                <div className='w-40 h-48 lg:w-96 lg:h-60 shrink rounded-[48px] bg-cover bg-center bg-size-mt-4' style={{backgroundImage: "url('/Webp/AdobeStock_469533192.webp')"}}/>
-              </div>
+          <div className='flex self-end  lg:self-center gap-4 pb-14'>
+            <div className='flex flex-col items-end'>
+              <div className='w-36 h-56 sm:w-40 sm:h-60 lg:w-44 lg:h-64 xl:w-48 xl:h-72 shrink rounded-[48px] bg-cover bg-no-repeat bg-[center_right_-5rem]' style={{backgroundImage: "url('/Webp/AdobeStock_280396247.webp')"}}/>
+              <div className='w-52 h-32 sm:w-60 sm:h-36 lg:w-64 lg:h-40 xl:w-72 xl:h-44 shrink rounded-[48px] bg-cover bg-no-repeat bg-left mt-4' style={{backgroundImage: "url('/Webp/AdobeStock_409017261.webp')"}}/>
             </div>
-            
+            <div className='flex flex-col items-start'>
+              <div className='w-40 h-40 sm:w-60 sm:h-48 lg:w-64 lg:h-52 xl:w-72 xl:h-56 shrink rounded-[48px] bg-cover bg-no-repeat bg-center' style={{backgroundImage: "url('/Webp/B-AdobeStock_235833811.webp')"}}/>
+              <div className='w-40 h-48 sm:w-72 sm:h-52 lg:w-80 lg:h-56 xl:w-96 xl:h-60 shrink rounded-[48px] bg-cover bg-no-repeat bg-center mt-4' style={{backgroundImage: "url('/Webp/AdobeStock_469533192.webp')"}}/>
+            </div>
+          </div> 
 
-          </div>
-            
         </div>
 
         {/*Second section: Explore - What We Do for You*/}
--- app/MainLayout.js
+++ app/MainLayout.js
@@ -10,11 +10,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function MainLayout({ showLinkBar, children }) {
     return (
       <html lang="en">
-         <body className={`bg-gray-200 flex flex-col min-h-screen ${inter.className}`}>
+         <body className={`flex flex-col min-h-screen ${inter.className}`}>
             {showLinkBar && <LinkBar/>}
               <NavBar/>
-          <main className='bg-gray-300 flex justify-center grow'>
-            <div className='bg-gray-400 w-full max-w-screen-2xl mx-auto p-6'>
+          <main className='flex grow'>
+            <div className='w-full max-w-screen-2xl mx-auto p-4'>
               {children}
             </div>
           </main>
