782c5a17779a399d6cc5aae69548cc55cc447f54
Hojoon Kim <hojoonkim.professional@gmail.com>
2023-11-27 22:57:15 -0800

    Solutions page:larger width view complete

--- app/(inner)/our-solutions/page.js
+++ app/(inner)/our-solutions/page.js
@@ -6,7 +6,7 @@ import BigButton from '@/components/BigButton';
 import { useState } from 'react';
 
 export default function OurSolutionsPage() {
-  const [activeAccordion, setActiveAccordion] = useState(null);
+  const [activeAccordion, setActiveAccordion] = useState('commercial');
 
   const handleAccordion = (id) => {
     if (activeAccordion === id) {
@@ -18,7 +18,10 @@ export default function OurSolutionsPage() {
 
   return (
     <main>
-      <div className='pb-8'>
+      
+      
+      {/* mobile display */}
+      <div className='sm:hidden pb-8'>
         <Heading
           heading= "For Businesses"
           title= "Explore our Solutions"
@@ -26,9 +29,9 @@ export default function OurSolutionsPage() {
         />
       </div>
       <div>
-        <div className='flex flex-col justify-center gap-4'>
+        <div className='sm:hidden flex flex-col my-auto justify-between gap-4'>
           {activeAccordion === 'commercial' && (
-            <div className='flex flex-col gap-4 pb-4'>
+            <div className='flex flex-col gap-4'>
               <div className='flex flex-col h-[262px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
                 <h3>Commercial Payments</h3>
                 <hr className='-mx-2'/>
@@ -42,7 +45,7 @@ export default function OurSolutionsPage() {
           <BigButton className='btn-blue' onClick={() => handleAccordion('commercial')}>Commercial Payments</BigButton>
           
           {activeAccordion === 'onlineAgent' && (
-            <div className='flex flex-col gap-4 pb-4'>
+            <div className='flex flex-col gap-4'>
               <div className='flex flex-col h-[262px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
                 <h3>Online Agent Platform</h3>
                 <hr className='-mx-2'/>
@@ -56,7 +59,7 @@ export default function OurSolutionsPage() {
           <BigButton className='btn-gray' onClick={() => handleAccordion('onlineAgent')}>Online Agent Platform</BigButton>
           
           {activeAccordion === 'payLink' && (
-            <div className='flex flex-col gap-4 pb-4'>
+            <div className='flex flex-col gap-4'>
               <div className='flex flex-col h-[262px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
                 <h3>Pay by Link</h3>
                 <hr className='-mx-2'/>
@@ -70,7 +73,7 @@ export default function OurSolutionsPage() {
           <BigButton className='btn-black' onClick={() => handleAccordion('payLink')}>Pay by Link</BigButton>
           
           {activeAccordion === 'unoKiosk' && (
-            <div className='flex flex-col gap-4 pb-4'>
+            <div className='flex flex-col gap-4'>
               <div className='flex flex-col h-[262px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
                 <h3>Uno Kiosk</h3>
                 <hr className='-mx-2'/>
@@ -83,6 +86,74 @@ export default function OurSolutionsPage() {
           )}
           <BigButton className='btn-green' onClick={() => handleAccordion('unoKiosk')}>Uno Kiosk</BigButton>
         </div>
+      </div>
+      
+      {/* normal display */}
+      <div className='hidden sm:flex flex-row gap-4 justify-center mx-auto'>
+        <div className='flex flex-col justify-between w-[612px]'>
+          <div className='flex pb-8'>
+            <Heading
+              heading= "For Businesses"
+              title= "Explore our Solutions"
+              detail= "Discover how our innovative solutions can simplify and enhance money transfer transactions by exploring our range of services."
+            />
+          </div>
+          {activeAccordion === 'commercial' && (
+            <div className='flex flex-col lg:flex-row-reverse gap-4 pb-4'>
+              <div className='flex flex-col min-h-max lg:max-w-[378px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
+                <h3>Commercial Payments</h3>
+                <hr className='-mx-2'/>
+                <p>Commercial Payments promotes financial inclusion and empower excluded small businesses by providing a secure cost effective solution for vendor payments.</p>
+              </div>
+              <div className='relative h-[237px] lg:h-[262px]'>
+                <img className='w-full h-full object-cover rounded-3xl' src="/Webp/AdobeStock_407334020.webp" alt="" width="100%"/>
+              </div>
+            </div>
+          )}
+          {activeAccordion === 'onlineAgent' && (
+            <div className='flex flex-col lg:flex-row-reverse gap-4 pb-4'>
+              <div className='flex flex-col min-h-max lg:max-w-[378px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
+                <h3>Online Agent Platform</h3>
+                <hr className='-mx-2'/>
+                <p>Omnex agents have access to our superior online management software designed to help agents efficiently assist their customers in sending money back home.</p>
+              </div>
+              <div className='relative h-[237px] lg:h-[262px]'>
+                <img className='w-full h-full object-cover rounded-3xl' src="/Webp/AdobeStock_469348689.webp" alt="" width="100%"/>
+              </div>
+            </div>
+          )}
+          {activeAccordion === 'payLink' && (
+            <div className='flex flex-col lg:flex-row-reverse gap-4 pb-4'>
+              <div className='flex flex-col min-h-max lg:max-w-[378px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
+                <h3>Pay by Link</h3>
+                <hr className='-mx-2'/>
+                <p>By offering convenient one-click money transfers through mobile phones, you can attract a larger customer base throughout your entire state and grow your business.</p>
+              </div>
+              <div className='relative h-[237px] lg:h-[262px]'>
+                <img className='w-full h-full object-cover rounded-3xl' src="/Webp/AdobeStock_574823710.webp" alt="" width="100%"/>
+              </div>
+            </div>
+          )}
+          {activeAccordion === 'unoKiosk' && (
+            <div className='flex flex-col lg:flex-row-reverse gap-4 pb-4'>
+              <div className='flex flex-col min-h-max lg:max-w-[378px] justify-between p-7 border border-[#52DB78] rounded-3xl'>
+                <h3>Uno Kiosk</h3>
+                <hr className='-mx-2'/>
+                <p>Uno Kiosk is a secure and hassle-free solution designed to help migrant workers easily send money to their loved ones, regardless of their physical location and transportation accessibility.</p>
+              </div>
+              <div className='relative h-[237px] lg:h-[283px]'>
+                <img className='w-full h-full object-cover rounded-3xl' src="/Webp/AdobeStock_579649494.webp" alt="" width="100%"/>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className='flex flex-col justify-end gap-4 pb-4'>
+          <BigButton className='btn-blue h-[236px]' onClick={() => handleAccordion('commercial')}>Commercial Payments</BigButton>
+          <BigButton className='btn-gray h-[65px]' onClick={() => handleAccordion('onlineAgent')}>Online Agent Platform</BigButton>
+          <BigButton className='btn-black h-[140px]' onClick={() => handleAccordion('payLink')}>Pay by Link</BigButton>
+          <BigButton className='btn-green h-[65px]' onClick={() => handleAccordion('unoKiosk')}>Uno Kiosk</BigButton>
+        </div>
+
       </div>
       
     </main>
--- app/components/BigButton.js
+++ app/components/BigButton.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Button({ children, onClick, className }) {
     return (
-      <div className={`${className} font-bold text-[22px] px-8 py-4 rounded-full min-w-max`} onClick={onClick}>
+      <div className={`${className} font-bold text-[22px] px-8 py-4 rounded-full sm:rounded-3xl min-w-max`} onClick={onClick}>
       {children}
       </div>
     );
