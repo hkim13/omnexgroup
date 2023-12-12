9fa34a102b4d238b60d34ad53ace281840cea3ba
Hojoon Kim <hojoonkim.professional@gmail.com>
2023-12-12 00:10:09 -0800

    state licenses page: state name, license url, license label complete

--- app/(inner)/regulatory/state-licenses/states.js
+++ app/(inner)/regulatory/state-licenses/states.js
@@ -26,7 +26,7 @@ const statesData = [
     {
         stateName: "Arkansas",
         agencyUrl: "https://securities.arkansas.gov/file-a-complaint-2/",
-        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-Arkansas-license.pdf",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-arkansas-license.pdf",
         licenseNumber: "20623",
         agencyLabel: "Arkansas Securities Department",
         licenseLabel: "Arkansas"
@@ -34,11 +34,366 @@ const statesData = [
     {
         stateName: "California",
         agencyUrl: "https://dfpi.ca.gov/submit-a-complaint/",
-        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-California-license.pdf",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-california-license.pdf",
         licenseNumber: "1646",
         agencyLabel: "Department of Financial Protection and Innovation Consumer Service Office",
         licenseLabel: "California"
     },
+
+
+    {
+        stateName: "Colorado",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-colorado-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Colorado"
+    },
+    {
+        stateName: "Connecticut",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-connecticut-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Connecticut"
+    },
+    {
+        stateName: "District of Columbia",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: ""
+    },
+    {
+        stateName: "Florida",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-florida-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Florida"
+    },
+    {
+        stateName: "Georgia",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-georgia-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Georgia"
+    },
+    {
+        stateName: "Hawaii",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-hawaii-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Hawaii"
+    },
+    {
+        stateName: "Idaho",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-idaho-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Idaho"
+    },
+    {
+        stateName: "Illinois",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-illinois-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Illinois"
+    },
+    {
+        stateName: "Indiana",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-indiana-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Indiana"
+    },
+    {
+        stateName: "Iowa",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-iowa-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Iowa"
+    },
+    {
+        stateName: "Kansas",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-kansas-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Kansas"
+    },
+    {
+        stateName: "Kentucky",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-kentucky-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Kentucky"
+    },
+    {
+        stateName: "Louisana",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-louisana-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Louisana"
+    },
+    {
+        stateName: "Maine",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-maine-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Maine"
+    },
+    {
+        stateName: "Maryland",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-maryland-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Maryland"
+    },
+    {
+        stateName: "Massachusetts",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-massachusetts-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Massachusetts"
+    },
+    {
+        stateName: "Michigan",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-michigan-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Michigan"
+    },
+    {
+        stateName: "Minnesota",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-minnesota-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Minnesota"
+    },
+    {
+        stateName: "Mississippi",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-mississippi-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Mississippi"
+    },
+    {
+        stateName: "Missouri",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-missouri-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Missouri"
+    },
+    {
+        stateName: "Montana",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-montana-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Montana"
+    },
+    {
+        stateName: "Nebraska",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-nebraska-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Nebraska"
+    },
+    {
+        stateName: "Nevada",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-nevada-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Nevada"
+    },
+    {
+        stateName: "New Hampshire",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-new-hampshire-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "New Hampshire"
+    },
+    {
+        stateName: "New Jersey",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-new-jersey-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "New Jersey"
+    },
+    {
+        stateName: "New Mexico",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-new-mexico-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "New Mexico"
+    },
+    {
+        stateName: "New York",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-new-york-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "New York"
+    },
+    {
+        stateName: "North Carolina",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-north-carolina-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "North Carolina"
+    },
+    {
+        stateName: "North Dakota",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-north-dakota-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "North Dakota"
+    },
+    {
+        stateName: "Ohio",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-ohio-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Ohio"
+    },
+    {
+        stateName: "Oregon",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-oregon-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Oregon"
+    },
+    {
+        stateName: "Pennsylvania",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-pennsylvania-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Pennsylvania"
+    },
+    {
+        stateName: "Rhode Island",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-rhode-island-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Rhode Island"
+    },
+    {
+        stateName: "South Carolina",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-south-carolina-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "South Carolina"
+    },
+    {
+        stateName: "South Dakota",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-south-dakota-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "South Dakota"
+    },
+    {
+        stateName: "Tennessee",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-tennessee-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Tennessee"
+    },
+    {
+        stateName: "Texas",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-texas-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Texas"
+    },
+    {
+        stateName: "Utah",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-utah-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Utah"
+    },
+    {
+        stateName: "Vermont",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-vermont-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Vermont"
+    },
+    {
+        stateName: "Virginia",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-virginia-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Virginia"
+    },
+    {
+        stateName: "Washington",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-washington-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Washington"
+    },
+    {
+        stateName: "West Virginia",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-west-virginia-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "West Virginia"
+    },
+    {
+        stateName: "Wisconsin",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-wisconsin-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Wisconsin"
+    },
+    {
+        stateName: "Wyoming",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-wyoming-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "Wyoming"
+    },
+    
     
   ];
   
