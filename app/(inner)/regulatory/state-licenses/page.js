0b14b53335394cb405802767707b0ea6cb2f2b6f
Hojoon Kim <hojoonkim.professional@gmail.com>
2023-12-11 17:57:47 -0800

    state licenses page: template functional

--- app/(inner)/regulatory/state-licenses/page.js
+++ app/(inner)/regulatory/state-licenses/page.js
@@ -3,15 +3,13 @@
 import Image from 'next/image'
 import Heading from '@/components/Heading'
 import { useState } from 'react';
+import statesData from './states';
 
 export default function StateLicenses() {
-  const [activeAccordion, setActiveAccordion] = useState(null);
+  const [activeState, setActiveState] = useState(null);
 
-  const handleAccordion = (id) => {
-    if (activeAccordion === id) {
-     return;
-    } 
-      setActiveAccordion(id);
+  const handleAccordion = (stateName) => {
+    setActiveState(activeState === stateName ? null : stateName);
   };
 
   return (
@@ -29,90 +27,51 @@ export default function StateLicenses() {
         <div className='hidden md:grid grid-cols-4 grid-rows-1 bg-[#253D84] border rounded-2xl place-content-between min-h-[91px] items-center text-[#FFFFFF]'>
           <div className='col-span-1 pl-[30px]'>State</div>      
           <div className='col-span-2'>Regulatory Agency</div>
-          <div className='col-span-1'>License</div>
+          <div className='col-span-1 pl-[30px]'>License</div>
         </div>
 
-        {/* Alabama */}
-        <div>
-          {/* mobile */}
-          <div className='md:hidden'>
-            <div className='bg-[#F5F5F5] flex flex-row border justify-between rounded-2xl h-[64px] items-center' onClick={() => handleAccordion('Alabama')}>
-              <div className=' pl-[30px] md:hidden'>Alabama</div>
-              <div className='w-[40px] h-[20px] md:hidden'>
-                <img className='pr-[24px] w-full h-full' src="/SVG/Group 154.png" alt="" />
+        {/* States */}
+        {statesData.map((state, index) => (
+          <div key={index}>
+            {/* Mobile View */}
+            <div className='md:hidden'>
+              <div className='bg-[#F5F5F5] flex flex-row border justify-between rounded-2xl h-[64px] items-center' onClick={() => handleAccordion(state.stateName)}>
+                <div className='pl-[30px]'>{state.stateName}</div>
+                <div className='w-[40px] h-[20px]'>
+                  <img className='pr-[24px] w-full h-full' src="/SVG/Group 154.png" alt="" />
+                </div>
+              </div>
+              {activeState === state.stateName && (
+                <div className='border rounded-2xl grid grid-cols-3 px-[30px] py-[25px] gap-4'>
+                  <div className='col-span-1'>State</div>
+                  <div className='col-span-2 font-bold'>{state.stateName}</div>
+                  <div className='col-span-1'>Regulatory Agency</div>
+                  <div className='col-span-2'>
+                    <a href={state.agencyUrl} className='font-bold' target="_blank" rel="noopener noreferrer">{state.agencyLabel}</a>
+                    <br/>License # {state.licenseNumber}
+                  </div>
+                  <div className='col-span-1'>License</div>
+                  <div className='col-span-2 font-bold'>
+                    <a href={state.licenseUrl} className='font-bold' target="_blank" rel="noopener noreferrer">{state.licenseLabel}</a>
+                  </div>
+                </div>  
+              )}
+            </div>
+
+            {/* Desktop View */}
+            <div className='hidden md:grid grid-cols-4 grid-rows-1 bg-[#F5F5F5] border rounded-2xl place-content-between min-h-[91px] items-center'>
+              <div className='col-span-1 font-bold text-[18px] pl-[30px]'>{state.stateName}</div>      
+              <div className='col-span-2'>
+                <a href={state.agencyUrl} className='font-bold' target="_blank" rel="noopener noreferrer">{state.agencyLabel}</a>
+                <br/>License # {state.licenseNumber}
+              </div>
+              <div className='col-span-1 pl-[30px]'>
+                <a href={state.licenseUrl} className='font-bold' target="_blank" rel="noopener noreferrer">{state.licenseLabel}</a>
               </div>
             </div>
-            {activeAccordion === 'Alabama' && (
-              <div className='border rounded-2xl grid grid-cols-3 px-[30px] py-[25px] gap-4 md:hidden'>
-                <div className='col-span-1'>State</div>
-                <div className='col-span-2 font-bold'>Alabama</div>
-                <div className='col-span-1'>Regulatory Agency</div>
-                <div className='col-span-2'>
-                  <a href="https://asc.alabama.gov/" className='font-bold' target="_blank" rel="noopener noreferrer">Alabama Securities Commission</a>
-                  <br/>License # SOC#306
-                </div>
-                <div className='col-span-1'>License</div>
-                <div className='col-span-2 font-bold'>
-                  <a href="https://www.sharemoney.com/assets/license/send-money-alabama-license.pdf" className='font-bold' target="_blank" rel="noopener noreferrer">Alabama</a>
-                </div>
-              </div>  
-            )}
           </div>
-
-          {/* desktop */}
-          <div className='hidden md:grid grid-cols-4 grid-rows-1 bg-[#F5F5F5] border rounded-2xl place-content-between min-h-[91px] items-center'>
-            <div className='col-span-1 font-bold text-[18px] pl-[30px]'>Alabama</div>      
-            <div className='col-span-2'>
-              <a href="https://asc.alabama.gov/" className='font-bold' target="_blank" rel="noopener noreferrer">Alabama Securities Commission</a>
-              <br/>License # SOC#306
-            </div>
-            <div className='col-span-1'>
-              <a href="https://www.sharemoney.com/assets/license/send-money-alabama-license.pdf" className='font-bold' target="_blank" rel="noopener noreferrer">Alabama</a>
-            </div>
-          </div>
-        </div>
-
-        {/* Arizona */}
-        <div>
-          {/* mobile */}
-          <div className='md:hidden'>
-            <div className='bg-[#F5F5F5] flex flex-row border justify-between rounded-2xl h-[64px] items-center' onClick={() => handleAccordion('Arizona')}>
-              <div className=' pl-[30px] md:hidden'>Arizona</div>
-              <div className='w-[40px] h-[20px] md:hidden'>
-                <img className='pr-[24px] w-full h-full' src="/SVG/Group 154.png" alt="" />
-              </div>
-            </div>
-            {activeAccordion === 'Arizona' && (
-              <div className='border rounded-2xl grid grid-cols-3 px-[30px] py-[25px] gap-4 md:hidden'>
-                <div className='col-span-1'>State</div>
-                <div className='col-span-2 font-bold'>Arizona</div>
-                <div className='col-span-1'>Regulatory Agency</div>
-                <div className='col-span-2'>
-                  <a href="https://asc.Arizona.gov/" className='font-bold' target="_blank" rel="noopener noreferrer">Arizona Securities Commission</a>
-                  <br/>License # SOC#306
-                </div>
-                <div className='col-span-1'>License</div>
-                <div className='col-span-2 font-bold'>
-                  <a href="https://www.sharemoney.com/assets/license/send-money-Arizona-license.pdf" className='font-bold' target="_blank" rel="noopener noreferrer">Arizona</a>
-                </div>
-              </div>  
-            )}
-          </div>
-
-          {/* desktop */}
-          <div className='hidden md:grid grid-cols-4 grid-rows-1 bg-[#F5F5F5] border rounded-2xl place-content-between min-h-[91px] items-center'>
-            <div className='col-span-1 font-bold text-[18px] pl-[30px]'>Arizona</div>      
-            <div className='col-span-2'>
-              <a href="https://asc.Arizona.gov/" className='font-bold' target="_blank" rel="noopener noreferrer">Arizona Securities Commission</a>
-              <br/>License # SOC#306
-            </div>
-            <div className='col-span-1'>
-              <a href="https://www.sharemoney.com/assets/license/send-money-Arizona-license.pdf" className='font-bold' target="_blank" rel="noopener noreferrer">Arizona</a>
-            </div>
-          </div>
-        </div>
-      
+        ))}
       </div>
     </main>
-  )
+  );
 }
---
+++ app/(inner)/regulatory/state-licenses/states.js
@@ -0,0 +1,45 @@
+const statesData = [
+    {
+        stateName: "National",
+        agencyUrl: "https://www.nmlsconsumeraccess.org/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-nmls-license.pdf",
+        licenseNumber: "899521",
+        agencyLabel: "National Multi-state Licensing System and Registration",
+        licenseLabel: "NMLS"
+    },
+    {
+        stateName: "Alabama",
+        agencyUrl: "https://asc.alabama.gov/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-alabama-license.pdf",
+        licenseNumber: "SOC#306",
+        agencyLabel: "Alabama Securities Commission",
+        licenseLabel: "Alabama"
+    },
+    {
+        stateName: "Arizona",
+        agencyUrl: "https://difi.az.gov/complaints",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-arizona-license.pdf",
+        licenseNumber: "MT-0904767",
+        agencyLabel: "Arizona Department of Financial Institutions Consumer Affairs Division",
+        licenseLabel: "Arizona"
+    },
+    {
+        stateName: "Arkansas",
+        agencyUrl: "https://securities.arkansas.gov/file-a-complaint-2/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-Arkansas-license.pdf",
+        licenseNumber: "20623",
+        agencyLabel: "Arkansas Securities Department",
+        licenseLabel: "Arkansas"
+    },
+    {
+        stateName: "California",
+        agencyUrl: "https://dfpi.ca.gov/submit-a-complaint/",
+        licenseUrl: "https://www.sharemoney.com/assets/license/send-money-California-license.pdf",
+        licenseNumber: "1646",
+        agencyLabel: "Department of Financial Protection and Innovation Consumer Service Office",
+        licenseLabel: "California"
+    },
+    
+  ];
+  
+  export default statesData;
