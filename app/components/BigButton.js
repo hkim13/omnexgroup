8979064316a0861d6a140e51a62bc1cfcd44dbae
Hojoon Kim <hojoonkim.professional@gmail.com>
2024-01-04 17:27:14 -0800

    removed duplicate app folder

--- app/components/BigButton.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Button({ children, onClick, className }) {
-    return (
-      <div className={`${className} font-bold text-[22px] px-8 py-4 rounded-full sm:rounded-3xl min-w-max`} onClick={onClick}>
-      {children}
-      </div>
-    );
-  }
--- app/components/Button.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Button({ children, className }) {
-    return (
-      <div className={`${className} font-medium px-6 py-2 rounded-full min-w-max`}>
-      {children}
-      </div>
-    );
-  }
