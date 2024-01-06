8ebc557fa1272d105231339b5a690116e5519e41
Hojoon Kim <hojoonkim.professional@gmail.com>
2024-01-05 21:19:33 -0800

    reverted next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withNextIntl = require('next-intl/plugin')();
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig
